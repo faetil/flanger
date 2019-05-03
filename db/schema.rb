f906f6a2f2a8742351264d596a3b6645317df88b
faetil <jaymeagalli@gmail.com>
2019-05-03 22:36:34 +1000

    changed to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_03_095531) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
     t.string "model"
